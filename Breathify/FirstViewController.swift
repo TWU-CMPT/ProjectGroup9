df51941438d44014c66ba0fd04ca582b74c45351
Hans Kim <hanskim@d207-023-196-243.wireless.sfu.ca>
2017-03-01 15:43:15 -0800

    Initial Commit

---
+++ Breathify/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Breathify
+//
+//  Created by Hans Kim on 2017-03-01.
+//  Copyright © 2017 Group Nein. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
