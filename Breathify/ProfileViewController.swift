c835e60a651c31a87850beb5aa8b6ce243aab1a5
pkrizia <pmacanla@d142-058-163-139.wireless.sfu.ca>
2017-03-12 19:33:25 -0700

    added tab and view controller for profile

---
+++ Breathify/ProfileViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ProfileViewController.swift
+//  Breathify
+//
+//  Created by Princess Macanlalay on 3/12/17.
+//  Copyright © 2017 Group Nein. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
