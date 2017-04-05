780ca9cf403b543586ddb72a997e6f126d2656d2
Keith Chan <keith.chan21@gmail.com>
2017-04-05 13:48:17 -0700

    deleting friends

--- Breathify/FriendsListTableViewController.swift
+++ Breathify/FriendsListTableViewController.swift
@@ -148,17 +148,21 @@ class FriendsListTableViewController: UITableViewController {
     }
     */
 
-    /*
+
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
+            
+            user.friendList.remove(at: indexPath.row)
+            Friends.remove(at: indexPath.row)
+            
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
+
 
     /*
     // Override to support rearranging the table view.
